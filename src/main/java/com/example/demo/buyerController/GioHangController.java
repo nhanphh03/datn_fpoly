024d68df1dbd69ae31b6bef6b40b4a8cfc6c58bb
NhanPham2003 <117698937+nhanphph23607@users.noreply.github.com>
2023-10-02 22:38:03 +0700

    Nhan Commit File

--- src/main/java/com/example/demo/buyerController/GioHangController.java
+++ src/main/java/com/example/demo/buyerController/GioHangController.java
@@ -1,4 +1,18 @@
 package com.example.demo.buyerController;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/offline")
 public class GioHangController {
+
+    @GetMapping("/index")
+    public String hienThi(){
+        return "offline/index";
+    }
+
+
 }
