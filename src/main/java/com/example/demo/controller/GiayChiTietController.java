a76e62b37bdb2f9bb5c6e3db6585fd0d15cfebb1
hunglv03 <Lvh9x8z5102003@gmail.com>
2023-10-03 17:10:23 +0700

    Load Table QL Sản Phẩm
    Load dữ liệu lên bảng

--- src/main/java/com/example/demo/controller/GiayChiTietController.java
+++ src/main/java/com/example/demo/controller/GiayChiTietController.java
@@ -5,18 +5,25 @@ import com.example.demo.service.GiayChiTietService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
-
+@RequestMapping("/manage")
 @Controller
 public class GiayChiTietController {
     @Autowired
     private GiayChiTietService giayChiTietService;
+
+    @ModelAttribute("dsTrangThai")
+    public Map<Integer, String> getDsTrangThai() {
+        Map<Integer, String> dsTrangThai = new HashMap<>();
+        dsTrangThai.put(1, "Hoạt động");
+        dsTrangThai.put(0, "Không hoạt động");
+        return dsTrangThai;
+    }
 
     @GetMapping("/giay-chi-tiet")
     public String dsGiayChiTiet(Model model) {
