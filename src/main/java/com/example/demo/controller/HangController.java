a0c4e5ca8f244ac0358ca497f3c5ab71ea821251
hunglv03 <Lvh9x8z5102003@gmail.com>
2023-10-05 00:02:06 +0700

    CRUD QL Sản Phẩm
    CRUD lên giao diện

--- src/main/java/com/example/demo/controller/HangController.java
+++ src/main/java/com/example/demo/controller/HangController.java
@@ -8,10 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
+
 @RequestMapping("/manage")
 @Controller
 public class HangController {
@@ -41,14 +39,19 @@ public class HangController {
 
     @PostMapping("/hang/viewAdd/add")
     public String addHang(@ModelAttribute("hang") Hang hang) {
-        hangService.save(hang);
-        return "redirect:/hang";
+        Hang hang1 = new Hang();
+        hang1.setMaHang(hang.getMaHang());
+        hang1.setTenHang(hang.getTenHang());
+        hang1.setTgThem(new Date());
+        hang1.setTrangThai(hang.getTrangThai());
+        hangService.save(hang1);
+        return "redirect:/manage/hang";
     }
 
     @GetMapping("/hang/delete/{id}")
     public String deleteHang(@PathVariable UUID id) {
         hangService.deleteByIdHang(id);
-        return "redirect:/hang";
+        return "redirect:/manage/hang";
     }
 
     @GetMapping("/hang/viewUpdate/{id}")
@@ -64,11 +67,10 @@ public class HangController {
         if (hangDb != null) {
             hangDb.setMaHang(hang.getMaHang());
             hangDb.setTenHang(hang.getTenHang());
-            hangDb.setTgSua(hang.getTgSua());
-            hangDb.setTgThem(hang.getTgThem());
+            hangDb.setTgSua(new Date());
             hangDb.setTrangThai(hang.getTrangThai());
             hangService.save(hangDb);
         }
-        return "redirect:/hang";
+        return "redirect:/manage/hang";
     }
 }
