bd0bfeac83179aab6636dcf0bcb4dd04974123d6
ngoclmph2496 <118239436+ngoclmph2496@users.noreply.github.com>
2023-10-27 21:57:23 +0700

    PDF VS EXCEL

---
+++ src/main/java/com/example/demo/config/ExcelExporterKhachHang.java
@@ -0,0 +1,99 @@
+package com.example.demo.config;
+
+import com.example.demo.model.KhachHang;
+import com.example.demo.model.NhanVien;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ExcelExporterKhachHang {
+    private XSSFWorkbook workbook;
+    private XSSFSheet sheet;
+    private List<KhachHang> listKhachHang;
+
+    public ExcelExporterKhachHang(List<KhachHang> listKhachHang) {
+        this.listKhachHang = listKhachHang;
+        workbook = new XSSFWorkbook();
+    }
+
+
+    private void writeHeaderLine() {
+        sheet = workbook.createSheet("Khách Hàng");
+
+        Row row = sheet.createRow(0);
+
+        CellStyle style = workbook.createCellStyle();
+        XSSFFont font = workbook.createFont();
+        font.setBold(true);
+        font.setFontHeight(16);
+        style.setFont(font);
+
+        createCell(row, 0, "ID", style);
+        createCell(row, 1, "Mã", style);
+        createCell(row, 2, "Tên Khách Hàng", style);
+        createCell(row, 3, "Loại Khách Hàng", style);
+        createCell(row, 5, "Trạng Thái", style);
+
+    }
+
+    private void createCell(Row row, int columnCount, Object value, CellStyle style) {
+        sheet.autoSizeColumn(columnCount);
+        Cell cell = row.createCell(columnCount);
+        if (value instanceof Integer) {
+            cell.setCellValue((Integer) value);
+        } else if (value instanceof Boolean) {
+            cell.setCellValue((Boolean) value);
+        } else {
+            cell.setCellValue((String) value);
+        }
+        cell.setCellStyle(style);
+    }
+
+    private void writeDataLines() {
+        int rowCount = 1;
+
+        CellStyle style = workbook.createCellStyle();
+        XSSFFont font = workbook.createFont();
+        font.setFontHeight(14);
+        style.setFont(font);
+
+        for (KhachHang khachHang : listKhachHang) {
+            Row row = sheet.createRow(rowCount++);
+            int columnCount = 0;
+
+            createCell(row, columnCount++, String.valueOf(khachHang.getIdKH()), style);
+            createCell(row, columnCount++, khachHang.getMaKH(), style);
+            createCell(row, columnCount++, khachHang.getHoTenKH(), style);
+            createCell(row, columnCount++, khachHang.getLoaiKhachHang().getTenLKH(), style);
+            if (khachHang.getTrangThai() == 1) {
+                createCell(row, columnCount++, "Hoạt động", style);
+            } else if (khachHang.getTrangThai() == 0) {
+                createCell(row, columnCount++, "Không hoạt động", style);
+            } else {
+                // Nếu giá trị trạng thái không phải là 0 hoặc 1, bạn có thể xử lý tùy ý ở đây.
+                createCell(row, columnCount++, "Giá trị không hợp lệ", style);
+            }
+
+        }
+    }
+
+    public void export(HttpServletResponse response) throws IOException {
+        writeHeaderLine();
+        writeDataLines();
+
+        ServletOutputStream outputStream = response.getOutputStream();
+        workbook.write(outputStream);
+        workbook.close();
+
+        outputStream.close();
+
+    }
+}
--- src/main/java/com/example/demo/controller/NhanVienController.java
+++ src/main/java/com/example/demo/controller/NhanVienController.java
@@ -1,13 +1,26 @@
 package com.example.demo.controller;
 
+import com.example.demo.config.ExcelExporterChatLieu;
+import com.example.demo.config.ExcelExporterNhanVien;
+import com.example.demo.config.PDFExporterChatLieu;
+import com.example.demo.config.PDFExporterNhanVien;
 import com.example.demo.model.*;
 import com.example.demo.service.ChucVuService;
 import com.example.demo.service.NhanVienService;
+import com.lowagie.text.DocumentException;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 @RequestMapping("manage")
@@ -37,6 +50,15 @@ public class NhanVienController {
     public String dsNhanVien(Model model) {
         List<NhanVien> nhanViens = nhanVienService.getAllNhanVien();
         List<ChucVu> chucVus = chucVuService.getAllChucVu();
+
+        // Kiểm tra và cập nhật trạng thái của giày nếu trạng thái của hãng hoặc chất liệu không hoạt động (0)
+        for (NhanVien nhanVienItem : nhanViens) {
+            if (nhanVienItem.getChucVu().getTrangThai() == 0) {
+                nhanVienItem.setTrangThai(0);
+                nhanVienService.save(nhanVienItem);
+            }
+        }
+        Collections.sort(nhanViens, (a, b) -> b.getTgThem().compareTo(a.getTgThem()));
         model.addAttribute("nhanVien", nhanViens);
         model.addAttribute("chucVu", chucVus);
         return "manage/nhan-vien";
@@ -44,17 +66,35 @@ public class NhanVienController {
 
     @GetMapping("/nhan-vien/viewAdd")
     public String viewAddNhanVien(Model model) {
-        List<ChucVu> chucVus = chucVuService.getAllChucVu();
+        List<ChucVu> chucVuList = chucVuService.getAllChucVu();
+        Collections.sort(chucVuList, (a, b) -> b.getTgThem().compareTo(a.getTgThem()));
+        model.addAttribute("chatVu", chucVuList);
+
         model.addAttribute("nhanVien", new NhanVien());
-        model.addAttribute("chucVu", chucVus);
+        model.addAttribute("chucVuAdd", new ChucVu());
         return "manage/add-nhan-vien";
     }
 
     @PostMapping("/nhan-vien/viewAdd/add")
-    public String addNhanVien(@ModelAttribute("nhanVien") NhanVien nhanVien) {
+    public String addNhanVien(@Valid @ModelAttribute("nhanVien") NhanVien nhanVien,Model model, BindingResult result) {
+       if(result.hasErrors()){
+           List<ChucVu> chucVuList = chucVuService.getAllChucVu();
+           Collections.sort(chucVuList, (a, b) -> b.getTgThem().compareTo(a.getTgThem()));
+           model.addAttribute("chucVu", chucVuList);
+
+           model.addAttribute("nhanVien", new NhanVien());
+           model.addAttribute("chucVuAdd", new ChucVu());
+           return "manage/add-nhan-vien";
+       }
         nhanVien.setTgThem(new Date());
         nhanVienService.save(nhanVien);
         return "redirect:/manage/nhan-vien";
+    }
+    @PostMapping("/nhan-vien/chuc-vu/viewAdd/add")
+    public String addChucVu(@ModelAttribute("chucVuAdd") ChucVu chucVu) {
+        chucVu.setTgThem(new Date());
+        chucVuService.save(chucVu);
+        return "redirect:/manage/nhan-vien/viewAdd";
     }
 
     @GetMapping("/nhan-vien/delete/{id}")
@@ -64,6 +104,12 @@ public class NhanVienController {
         nhanVien.setTgSua(new Date());
         nhanVienService.save(nhanVien);
         return "redirect:/manage/nhan-vien";
+    }
+    public void deleteNVById(UUID idNV) {
+        NhanVien nhanVien = nhanVienService.getByIdNhanVien(idNV);
+        nhanVien.setTrangThai(0);
+        nhanVien.setTgSua(new Date());
+        nhanVienService.save(nhanVien);
     }
 
     @GetMapping("/nhan-vien/viewUpdate/{id}")
@@ -96,4 +142,70 @@ public class NhanVienController {
         }
         return "redirect:/manage/nhan-vien";
     }
+    @GetMapping("/nhanVien/export/pdf")
+    public void exportToPDFChatLieu(HttpServletResponse response) throws DocumentException, IOException {
+        response.setContentType("application/pdf");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=nhanVien_" + currentDateTime + ".pdf";
+        response.setHeader(headerKey, headerValue);
+
+        List<NhanVien> listNhanVien = nhanVienService.getAllNhanVien();
+
+        PDFExporterNhanVien exporter = new PDFExporterNhanVien(listNhanVien);
+        exporter.export(response);
+    }
+
+    @GetMapping("/nhanVien/export/excel")
+    public void exportToExcelSize(HttpServletResponse response) throws IOException {
+        response.setContentType("application/octet-stream");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=nhanVien_" + currentDateTime + ".xlsx";
+        response.setHeader(headerKey, headerValue);
+
+        List<NhanVien> listNhanVien = nhanVienService.getAllNhanVien();
+
+        ExcelExporterNhanVien excelExporter = new ExcelExporterNhanVien(listNhanVien);
+
+        excelExporter.export(response);
+    }
+
+    @GetMapping("/nhanVien/filter")
+    public String filterData(Model model,
+                             @RequestParam(value = "maNV", required = false) String maNV,
+                             @RequestParam(value = "tenNV", required = false) String tenNV) {
+        // Thực hiện lọc dữ liệu dựa trên selectedSize (và trạng thái nếu cần)
+        List<NhanVien> filteredNhanViens;
+        if ("Mã Nhân Viên".equals(maNV) && "Tên Nhân Viên".equals(tenNV)) {
+            // Nếu người dùng chọn "Tất cả", hiển thị tất cả dữ liệu
+            filteredNhanViens = nhanVienService.getAllNhanVien();
+        } else {
+            // Thực hiện lọc dữ liệu dựa trên selectedSize
+            filteredNhanViens = nhanVienService.fillterNhanVien(maNV, tenNV);
+        }
+        model.addAttribute("nhanVien", filteredNhanViens);
+        model.addAttribute("nhanVienAll", nhanVienService.getAllNhanVien());
+
+        return "manage/nhan-vien"; // Trả về mẫu HTML chứa bảng dữ liệu sau khi lọc
+    }
+
+    @PostMapping("/nhanVien/import")
+    public String importData(@RequestParam("file") MultipartFile file) {
+        if (file != null && !file.isEmpty()) {
+            try {
+                InputStream excelFile = file.getInputStream();
+                nhanVienService.importDataFromExcel(excelFile); // Gọi phương thức nhập liệu từ Excel
+            } catch (Exception e) {
+                e.printStackTrace();
+                // Xử lý lỗi
+            }
+        }
+        return "redirect:/manage/nhan-vien"; // Chuyển hướng sau khi nhập liệu thành công hoặc không thành công
+    }
+
 }
